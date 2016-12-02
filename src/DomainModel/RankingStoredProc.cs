4ca24001058145a39d5d4edd6cb6d497bd58db83
Monia <Monia@MONIA-HP>
2016-12-02 14:32:43 +0100

    Add project files.

---
+++ src/DomainModel/RankingStoredProc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class RankingStoredProc
+    {
+        [Key]
+        [Column("posts.id")]
+        public int postsid { get; set; }
+        [Column("Rank")]
+        public decimal? Rank { get; set; }
+        [Column("body")]
+        public string body { get; set; }
+        
+    }
+}
